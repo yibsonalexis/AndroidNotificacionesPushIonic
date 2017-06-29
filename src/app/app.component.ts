9c1c6dcf1a7f06687d82ca7eec237ea09bdaf9f0
Yibson Leudo <roley_012@hotmail.com>
2017-06-29 11:27:30 -0500

    Notificaciones Push Android usando Ionic
    Notificaciones push con ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,54 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public push: Push
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.RegisterNotification();
+      this.Notification();
+
+
     });
   }
+
+  private RegisterNotification() {
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    });
+  }
+
+  private Notification() {
+    this.push.rx.notification()
+      .subscribe((msg) => {
+        alert(msg.title + ': ' + msg.text);
+      });
+  }
+
+
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,28 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'a0a1a3cd',
+  },
+  'push': {
+    'sender_id': '1024866608153',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +34,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
